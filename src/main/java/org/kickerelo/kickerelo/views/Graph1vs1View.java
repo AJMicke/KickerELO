d2c1f8eb2db352bbbd6860272f2d76d145dbbc6f
Anton Micke <anton.micke@gmail.com>
2025-03-18 16:52:17 +0100

    Fix size of elo graphs

--- src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
@@ -1,20 +1,24 @@
 package org.kickerelo.kickerelo.views;
 
-import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.ApexCharts;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import org.kickerelo.kickerelo.service.KickerEloService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("graph1vs1")
 public class Graph1vs1View extends VerticalLayout {
 
-    ApexChartsBuilder chart1vs1;
+    ApexCharts chart1vs1;
     public Graph1vs1View(KickerEloService service) {
+        setSizeFull();
         H2 subheading = new H2("1 vs 1 Elo");
-        chart1vs1 = new Chart1vs1(service.getSpielerEntities());
-        add(subheading, chart1vs1.build());
+        chart1vs1 = new Chart1vs1(service.getSpielerEntities()).build();
+        chart1vs1.setWidth(100, Unit.PERCENTAGE);
+        chart1vs1.setHeight(100, Unit.PERCENTAGE);
+
+        add(subheading, chart1vs1);
     }
 
 }
