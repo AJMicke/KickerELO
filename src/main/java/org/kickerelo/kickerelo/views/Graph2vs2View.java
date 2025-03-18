6d220f53c1ed6e324c801e4dbeac853f39bef3e5
Anton Micke <anton.micke@gmail.com>
2025-03-18 16:52:17 +0100

    Fix size of elo graphs

--- src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
@@ -1,6 +1,7 @@
 package org.kickerelo.kickerelo.views;
 
-import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.ApexCharts;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -8,10 +9,14 @@ import org.kickerelo.kickerelo.service.KickerEloService;
 
 @Route("graph2vs2")
 public class Graph2vs2View extends VerticalLayout {
-    ApexChartsBuilder chart2vs2;
+    ApexCharts chart2vs2;
     public Graph2vs2View(KickerEloService service) {
+        setSizeFull();
         H2 subheading = new H2("2 vs 2 Elo");
-        chart2vs2 = new Chart2vs2(service.getSpielerEntities());
-        add(subheading, chart2vs2.build());
+        chart2vs2 = new Chart2vs2(service.getSpielerEntities()).build();
+        chart2vs2.setWidth(100, Unit.PERCENTAGE);
+        chart2vs2.setHeight(100, Unit.PERCENTAGE);
+
+        add(subheading, chart2vs2);
     }
 }
