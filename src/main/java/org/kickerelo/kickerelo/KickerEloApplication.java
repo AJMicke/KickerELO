e0791a7cb87abb83a51e9df9e57ab4e0a7aecf8a
Anton Micke <anton.micke@gmail.com>
2025-03-21 20:54:14 +0100

    Enable PWA

--- src/main/java/org/kickerelo/kickerelo/KickerEloApplication.java
+++ src/main/java/org/kickerelo/kickerelo/KickerEloApplication.java
@@ -1,6 +1,7 @@
 package org.kickerelo.kickerelo;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +13,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "org.kickerelo.kickerelo.data")
 @EnableJpaRepositories
 @Theme("my-theme")
+@PWA(name = "Tischkicker-Elo", shortName = "KickerElo",
+        description = "Erlaubt Hinzufügen von Spielständen und Tracking der Spieler Elos für 1 vs 1 und 2 vs 2")
 public class KickerEloApplication implements AppShellConfigurator {
     public static void main(String[] args) {
         SpringApplication.run(KickerEloApplication.class, args);
