c2c9fbf3d746dffe2c587c2d809933d14f2e9873
Anton Micke <anton.micke@gmail.com>
2025-03-21 20:20:54 +0100

    Add filtering to match history

--- src/main/java/org/kickerelo/kickerelo/repository/Ergebnis1vs1Repository.java
+++ src/main/java/org/kickerelo/kickerelo/repository/Ergebnis1vs1Repository.java
@@ -2,8 +2,20 @@ package org.kickerelo.kickerelo.repository;
 
 import org.kickerelo.kickerelo.data.Ergebnis1vs1;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface Ergebnis1vs1Repository extends JpaRepository<Ergebnis1vs1, Long> {
+    @Query("""
+        SELECT e from Ergebnis1vs1 e WHERE
+        (:S1 IS NOT NULL AND :S2 IS NOT NULL AND (:S1=e.gewinner.name AND :S2 = e.verlierer.name OR :S1=e.verlierer.name AND :S2=e.gewinner.name)) OR
+        (:S1 IS NOT NULL AND :S2 IS NULL AND (:S1=e.gewinner.name OR :S1=e.verlierer.name)) OR
+        (:S1 IS NULL AND :S2 IS NOT NULL AND (:S2=e.gewinner.name OR :S2=e.verlierer.name)) OR
+        (:S1 IS NULL AND :S2 IS NULL)
+    """)
+    List<Ergebnis1vs1> getFiltered(@Param("S1") String s1, @Param("S2") String s2);
 }
--- src/main/java/org/kickerelo/kickerelo/views/History2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/History2vs2View.java
@@ -2,10 +2,16 @@ package org.kickerelo.kickerelo.views;
 
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridSortOrder;
+import com.vaadin.flow.component.grid.dataview.GridListDataView;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.provider.SortDirection;
 import com.vaadin.flow.data.renderer.LocalDateTimeRenderer;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.kickerelo.kickerelo.data.Ergebnis2vs2;
 import org.kickerelo.kickerelo.repository.Ergebnis2vs2Repository;
@@ -19,10 +25,26 @@ public class History2vs2View extends VerticalLayout {
         H2 subheading = new H2("Spiele 2 vs 2");
         List<Ergebnis2vs2> res = repo.findAll();
         Grid<Ergebnis2vs2> grid = new Grid<>(Ergebnis2vs2.class);
+        GridListDataView<Ergebnis2vs2> dataView = grid.setItems(res);
 
-        grid.setItems(res);
+        HorizontalLayout filterLayout = new HorizontalLayout();
+        filterLayout.setWidth("95%");
+        TextField filter1 = new TextField();
+        filter1.setPlaceholder("Spieler");
+        filter1.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
+        filter1.setValueChangeMode(ValueChangeMode.EAGER);
+        filter1.addValueChangeListener(event -> dataView.refreshAll());
+        filter1.setMaxWidth("50%");
+        TextField filter2 = new TextField();
+        filter2.setPlaceholder("Spieler");
+        filter2.setPrefixComponent(new Icon(VaadinIcon.SEARCH));
+        filter2.setValueChangeMode(ValueChangeMode.EAGER);
+        filter2.addValueChangeListener(event -> dataView.refreshAll());
+        filter2.setMaxWidth("50%");
+
+        filterLayout.add(filter1, filter2);
+
         grid.removeColumnByKey("id");
-
         Grid.Column<Ergebnis2vs2> winnerFront = grid.getColumnByKey("gewinnerVorn");
         winnerFront.setHeader("Gewinner vorne");
         Grid.Column<Ergebnis2vs2> winnerBack = grid.getColumnByKey("gewinnerHinten");
@@ -40,6 +62,34 @@ public class History2vs2View extends VerticalLayout {
         grid.setColumnOrder(winnerFront, winnerBack, loserFront, loserBack, goals, timestamp);
         GridSortOrder<Ergebnis2vs2> sortOrder = new GridSortOrder<>(timestamp, SortDirection.DESCENDING);
         grid.sort(List.of(sortOrder));
-        add(subheading, grid);
+
+        dataView.addFilter(result -> {
+            String name1 = result.getGewinnerVorn().getName();
+            String name2 = result.getVerliererVorn().getName();
+            String name3 = result.getVerliererHinten().getName();
+            String name4 = result.getGewinnerHinten().getName();
+            String s1 = filter1.getValue();
+            boolean p1 = !(s1 == null || s1.isEmpty());
+            String s2 = filter2.getValue();
+            boolean p2 = !(s2 == null || s2.isEmpty());
+
+            if (p1 && p2) {
+                return name1.contains(s1) && name2.contains(s2) || name2.contains(s1) && name1.contains(s2)
+                    || name1.contains(s1) && name3.contains(s2) || name3.contains(s1) && name1.contains(s2)
+                    || name1.contains(s1) && name4.contains(s2) || name4.contains(s1) && name1.contains(s2)
+                    || name2.contains(s1) && name3.contains(s2) || name3.contains(s1) && name2.contains(s2)
+                    || name2.contains(s1) && name4.contains(s2) || name4.contains(s1) && name2.contains(s2)
+                    || name3.contains(s1) && name4.contains(s2) || name4.contains(s1) && name3.contains(s2);
+            }
+            if (p1) {
+                return name1.contains(s1) || name2.contains(s1) || name3.contains(s1) || name4.contains(s1);
+            }
+            if (p2) {
+                return name1.contains(s2) || name2.contains(s2) || name3.contains(s2) || name4.contains(s2);
+            }
+            return true;
+        });
+
+        add(subheading, filterLayout, grid);
     }
 }
