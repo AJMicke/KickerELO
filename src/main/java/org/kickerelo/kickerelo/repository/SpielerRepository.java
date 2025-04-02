83d1101563f39f9483e156d9ee1ebcf71e02b0d6
Anton Micke <anton.micke@gmail.com>
2025-04-02 14:28:25 +0200

    In the graphs, show only players with actual games in the category

--- src/main/java/org/kickerelo/kickerelo/repository/SpielerRepository.java
+++ src/main/java/org/kickerelo/kickerelo/repository/SpielerRepository.java
@@ -2,11 +2,20 @@ package org.kickerelo.kickerelo.repository;
 
 import org.kickerelo.kickerelo.data.Spieler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SpielerRepository extends JpaRepository<Spieler, Long> {
     Optional<Spieler> findByName(String name);
+
+    @Query("SELECT s FROM Spieler s WHERE EXISTS (SELECT 1 FROM Ergebnis1vs1 e WHERE e.gewinner=s OR e.verlierer=s)")
+    List<Spieler> getSpielerWith1vs1Games();
+
+    @Query("SELECT s FROM Spieler s WHERE EXISTS (SELECT 1 FROM Ergebnis2vs2 e " +
+            "WHERE e.gewinnerVorn=s OR e.verliererVorn=s OR e.gewinnerHinten=s OR e.verliererHinten=s)")
+    List<Spieler> getSpielerWith2vs2Games();
 }
--- src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
@@ -5,15 +5,15 @@ import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.kickerelo.kickerelo.service.KickerEloService;
+import org.kickerelo.kickerelo.repository.SpielerRepository;
 
 @Route("graph2vs2")
 public class Graph2vs2View extends VerticalLayout {
     ApexCharts chart2vs2;
-    public Graph2vs2View(KickerEloService service) {
+    public Graph2vs2View(SpielerRepository repo) {
         setSizeFull();
         H2 subheading = new H2("2 vs 2 Elo");
-        chart2vs2 = new Chart2vs2(service.getSpielerEntities()).build();
+        chart2vs2 = new Chart2vs2(repo.getSpielerWith2vs2Games()).build();
         chart2vs2.setWidth(100, Unit.PERCENTAGE);
         chart2vs2.setHeight(100, Unit.PERCENTAGE);
 
