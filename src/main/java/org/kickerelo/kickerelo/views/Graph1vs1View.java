975a27a95b8943e3fa7493bf307f4aec98cd96fb
Anton Micke <anton.micke@gmail.com>
2025-04-02 14:28:25 +0200

    In the graphs, show only players with actual games in the category

--- src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
@@ -5,20 +5,19 @@ import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.kickerelo.kickerelo.service.KickerEloService;
+import org.kickerelo.kickerelo.repository.SpielerRepository;
 
 @Route("graph1vs1")
 public class Graph1vs1View extends VerticalLayout {
 
     ApexCharts chart1vs1;
-    public Graph1vs1View(KickerEloService service) {
+    public Graph1vs1View(SpielerRepository repo) {
         setSizeFull();
         H2 subheading = new H2("1 vs 1 Elo");
-        chart1vs1 = new Chart1vs1(service.getSpielerEntities()).build();
+        chart1vs1 = new Chart1vs1(repo.getSpielerWith1vs1Games()).build();
         chart1vs1.setWidth(100, Unit.PERCENTAGE);
         chart1vs1.setHeight(100, Unit.PERCENTAGE);
 
         add(subheading, chart1vs1);
     }
-
 }
