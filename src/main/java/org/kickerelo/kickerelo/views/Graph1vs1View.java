fe43923df82e5d9d81acc94d517350b0026f908f
Leon Marz <main@lmarz.org>
2025-05-12 17:43:35 +0200

    Switch to chart.js

--- src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
@@ -1,23 +1,29 @@
 package org.kickerelo.kickerelo.views;
 
-import com.github.appreciated.apexcharts.ApexCharts;
-import com.vaadin.flow.component.Unit;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
 import org.kickerelo.kickerelo.repository.SpielerRepository;
+import org.kickerelo.kickerelo.util.Spieler1vs1EloComparator;
 
 @Route("graph1vs1")
 public class Graph1vs1View extends VerticalLayout {
 
-    ApexCharts chart1vs1;
     public Graph1vs1View(SpielerRepository repo) {
         setSizeFull();
         H2 subheading = new H2("1 vs 1 Elo");
-        chart1vs1 = new Chart1vs1(repo.getSpielerWith1vs1Games()).build();
-        chart1vs1.setWidth(100, Unit.PERCENTAGE);
-        chart1vs1.setHeight(100, Unit.PERCENTAGE);
 
-        add(subheading, chart1vs1);
+        List<String> names = new ArrayList();
+        List<Float> elo = new ArrayList();
+
+        repo.getSpielerWith1vs1Games().stream().sorted(new Spieler1vs1EloComparator()).forEach((s) -> {names.add(s.getName()); elo.add(s.getElo1vs1());});
+
+        Chart chart = new Chart(names, elo);
+
+        add(subheading, chart);
     }
 }
