c25fe2789b8f7d2c7dfb4623874db258d08e921d
lmarz <main@lmarz.org>
2025-05-14 14:15:49 +0200

    Switch to chart.js

---
+++ src/main/java/org/kickerelo/kickerelo/views/Chart.java
@@ -0,0 +1,45 @@
+package org.kickerelo.kickerelo.views;
+
+import java.util.List;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.Tag;
+
+import org.json.JSONArray;
+
+@Tag("canvas")
+public class Chart extends Component {
+
+    public Chart(List<String> xvalues, List<Float> yvalues) {
+        setId("chart");
+
+        UI.getCurrent().getPage().addJavaScript("https://cdn.jsdelivr.net/npm/chart.js");
+
+        JSONArray x = new JSONArray(xvalues);
+        JSONArray y = new JSONArray(yvalues);
+
+        String js = "";
+
+        // Dark mode setting
+        js += "if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {";
+        js +=     "Chart.defaults.color = 'hsla(214, 96%, 96%, 0.9)';";
+        js +=     "Chart.defaults.borderColor = 'hsla(214, 78%, 88%, 0.5)';";
+        js += "}";
+
+        // Font size
+        js += "Chart.defaults.font.size = 16;";
+
+        // Chart
+        js += "new Chart(document.getElementById('chart'), {type: 'line', ";
+        js += "options: {showLine: false, pointRadius: 7, plugins: { legend: { display: false}}, layout: { padding: 10}}, ";
+
+        // Data
+        js += "data: { labels: " + x + ", datasets:[{data: " + y + ", ";
+        js += "borderColor: 'hsl(214, 90%, 48%)', backgroundColor: 'hsl(214, 90%, 77%)'}]}});";
+
+        getElement().executeJs(js);
+
+    }
+
+}
--- src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
@@ -1,22 +1,29 @@
 package org.kickerelo.kickerelo.views;
 
-import com.github.appreciated.apexcharts.ApexCharts;
-import com.vaadin.flow.component.Unit;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
 import org.kickerelo.kickerelo.repository.SpielerRepository;
+import org.kickerelo.kickerelo.util.Spieler2vs2EloComparator;
 
 @Route("graph2vs2")
 public class Graph2vs2View extends VerticalLayout {
-    ApexCharts chart2vs2;
+
     public Graph2vs2View(SpielerRepository repo) {
         setSizeFull();
         H2 subheading = new H2("2 vs 2 Elo");
-        chart2vs2 = new Chart2vs2(repo.getSpielerWith2vs2Games()).build();
-        chart2vs2.setWidth(100, Unit.PERCENTAGE);
-        chart2vs2.setHeight(100, Unit.PERCENTAGE);
 
-        add(subheading, chart2vs2);
+        List<String> names = new ArrayList<>();
+        List<Float> elo = new ArrayList<>();
+
+        repo.getSpielerWith2vs2Games().stream().sorted(new Spieler2vs2EloComparator()).forEach((s) -> {names.add(s.getName()); elo.add(s.getElo2vs2());});
+
+        Chart chart = new Chart(names, elo);
+
+        add(subheading, chart);
     }
 }
