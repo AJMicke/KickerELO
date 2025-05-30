1acccc70dcdc054f19a134d3c4227ff247183e3b
Anton Micke <anton.micke@gmail.com>
2025-05-30 15:10:27 +0200

    Add average goal diffs to 2vs2 stats

--- src/main/java/org/kickerelo/kickerelo/repository/Ergebnis2vs2Repository.java
+++ src/main/java/org/kickerelo/kickerelo/repository/Ergebnis2vs2Repository.java
@@ -3,6 +3,8 @@ package org.kickerelo.kickerelo.repository;
 import org.kickerelo.kickerelo.data.Ergebnis2vs2;
 import org.kickerelo.kickerelo.data.Spieler;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,4 +18,10 @@ public interface Ergebnis2vs2Repository extends JpaRepository<Ergebnis2vs2, Long
                                                                             Spieler verliererVon, Spieler verliererHinten);
 
     int countByGewinnerVornOrVerliererVorn(Spieler gewinnerVon, Spieler verliererHinten);
+
+    @Query("select avg(case when e.verliererVorn = :s then (e.toreVerlierer - 10) when e.gewinnerVorn = :s then (10 - e.toreVerlierer) end) from Ergebnis2vs2 e where e.gewinnerVorn = :s or e.verliererVorn = :s")
+    Float avgGoalDiffFront(@Param("s") Spieler s);
+
+    @Query("select avg(case when e.verliererHinten = :s then (e.toreVerlierer - 10) when e.gewinnerHinten = :s then (10 - e.toreVerlierer) end) from Ergebnis2vs2 e where e.gewinnerHinten = :s or e.verliererHinten = :s")
+    Float avgGoalDiffBack(@Param("s") Spieler s);
 }
