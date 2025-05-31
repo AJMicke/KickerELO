f0bf51bf4e7d5338afda55c99aedb159a94b7090
Moritz Kowalski <Moritz-Kowalski@gmx.de>
2025-05-31 18:55:34 +0200

    Small cleanup

--- src/main/java/org/kickerelo/kickerelo/config/SecurityConfig.java
+++ src/main/java/org/kickerelo/kickerelo/config/SecurityConfig.java
@@ -7,27 +7,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
-    // unnecessary, because already configured in application.properties
-    // private final Dotenv dotenv = Dotenv.load();
-    // @Bean
-    // public ClientRegistrationRepository clientRegistrationRepository() {
-    //     ClientRegistration oidcRegistration = ClientRegistration.withRegistrationId("oidc")
-    //         .clientId(dotenv.get("OIDC_CLIENT_ID"))
-    //         .clientSecret(dotenv.get("OIDC_CLIENT_SECRET"))
-    //         .scope("openid", "profile", "email")
-    //         .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-    //         .authorizationUri(dotenv.get("OIDC_ISSUER_BASE_URI") + "application/o/authorize/")
-    //         .tokenUri(dotenv.get("OIDC_ISSUER_BASE_URI") + "application/o/token/")
-    //         .userInfoUri(dotenv.get("OIDC_ISSUER_BASE_URI") + "application/o/userinfo/")
-    //         .userNameAttributeName("sub")
-    //         .clientName("OIDC")
-    //         .redirectUri(dotenv.get("OIDC_REDIRECT_URI"))
-    //         .build();
-
-    //     return new InMemoryClientRegistrationRepository(oidcRegistration);
-    // }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
