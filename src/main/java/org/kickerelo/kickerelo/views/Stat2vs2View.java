6ba5b7d59ecb033eac401509baa2a3f6f17d6251
Moritz <34131471+Moritz921@users.noreply.github.com>
2025-06-13 15:06:41 +0200

    Add OIDC support again (#62)
    * Update readme
    
    * First try in oidc implementation
    
    * Add secrets
    
    * Add connection to fs auth provider, redirect not tested
    
    * Working prototype
    
    This commit implements the oidc compatibility with the caveat of having
    every subsite under the app path. For that, there is also a redirection
    handler to redirect the home page to the app home page.
    
    * Small cleanup
    
    * Fix access even when logged in
    
    * Update application-prod.properties
    
    * Ignore login when in test env
    
    * Fix reviews
    
    * Feature/sign in (#1)
    
    * Add OIDC support (#39)
    
    * Update readme
    
    * First try in oidc implementation
    
    * Add secrets
    
    * Add connection to fs auth provider, redirect not tested
    
    * Working prototype
    
    This commit implements the oidc compatibility with the caveat of having
    every subsite under the app path. For that, there is also a redirection
    handler to redirect the home page to the app home page.
    
    * Small cleanup
    
    * Fix access even when logged in
    
    * Update application-prod.properties
    
    * Ignore login when in test env
    
    * Fix reviews
    
    * Revert "Add OIDC support (#39)" (#60)
    
    This reverts commit 244f6cbf9590c081c2e10a5d93d9b942ec751c0a.
    
    * Refine the sign in solution by Moritz921
    
    * Fix botched rebase
    
    ---------
    
    Co-authored-by: AJMicke <7047945+AJMicke@users.noreply.github.com>
    Co-authored-by: Anton Micke <anton.micke@gmail.com>
    
    * Fix bugs
    
    ---------
    
    Co-authored-by: AJMicke <7047945+AJMicke@users.noreply.github.com>
    Co-authored-by: Anton Micke <anton.micke@gmail.com>

--- src/main/java/org/kickerelo/kickerelo/views/Stat2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Stat2vs2View.java
@@ -1,5 +1,11 @@
 package org.kickerelo.kickerelo.views;
 
+import org.kickerelo.kickerelo.data.Spieler;
+import org.kickerelo.kickerelo.repository.Ergebnis2vs2Repository;
+import org.kickerelo.kickerelo.service.KickerEloService;
+import org.kickerelo.kickerelo.service.Stat2vs2Service;
+import org.kickerelo.kickerelo.util.Position;
+
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.NativeLabel;
@@ -8,11 +14,6 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.progressbar.ProgressBar;
 import com.vaadin.flow.component.progressbar.ProgressBarVariant;
 import com.vaadin.flow.router.Route;
-import org.kickerelo.kickerelo.data.Spieler;
-import org.kickerelo.kickerelo.repository.Ergebnis2vs2Repository;
-import org.kickerelo.kickerelo.service.KickerEloService;
-import org.kickerelo.kickerelo.service.Stat2vs2Service;
-import org.kickerelo.kickerelo.util.Position;
 
 @Route("stat2vs2")
 public class Stat2vs2View extends VerticalLayout {
