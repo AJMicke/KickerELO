0d7c6feb95f8a0f4a5cef9b463dea2d5b3bbf1ae
Anton Micke <anton.micke@gmail.com>
2025-06-27 14:41:48 +0200

    Next try for remember me

---
+++ src/main/java/org/kickerelo/kickerelo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package org.kickerelo.kickerelo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.oauth2.client.JdbcOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+
+@Profile("prod")
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public OAuth2AuthorizedClientService authorizedClientService(
+            JdbcTemplate jdbcTemplate,
+            ClientRegistrationRepository clientRegistrationRepository) {
+        return new JdbcOAuth2AuthorizedClientService(jdbcTemplate, clientRegistrationRepository);
+    }
+}
