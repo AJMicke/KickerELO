d395b4cb186007dbdb98f7b524863d25cf1983e3
Sebastian Beckmann <beckmann.sebastian@outlook.de>
2025-09-09 00:12:51 +0200

    data: Store user who submitted the game
    This may become useful in the future.

--- src/main/java/org/kickerelo/kickerelo/data/AuthentikUser.java
+++ src/main/java/org/kickerelo/kickerelo/data/AuthentikUser.java
@@ -3,6 +3,8 @@ package org.kickerelo.kickerelo.data;
 import jakarta.persistence.*;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Entity
@@ -18,12 +20,20 @@ public class AuthentikUser {
     @Column(name = "NAME", nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "submitter", cascade = CascadeType.ALL)
+    private List<Ergebnis1vs1> submittedGames1v1;
+
+    @OneToMany(mappedBy = "submitter", cascade = CascadeType.ALL)
+    private List<Ergebnis2vs2> submittedGames2v2;
+
     public AuthentikUser() {
     }
 
     public AuthentikUser(String id, String name) {
         this.id = id;
         this.name = name;
+        this.submittedGames1v1 = new ArrayList<>();
+        this.submittedGames2v2 = new ArrayList<>();
     }
 
     public String getName() {
@@ -36,6 +46,14 @@ public class AuthentikUser {
 
     public void setSpieler(@Nullable Spieler spieler) {
         this.spieler = spieler;
+    }
+
+    public void addSubmittedResult(Ergebnis1vs1 ergebnis) {
+        submittedGames1v1.add(ergebnis);
+    }
+
+    public void addSubmittedResult(Ergebnis2vs2 ergebnis) {
+        submittedGames2v2.add(ergebnis);
     }
 
     @Override
