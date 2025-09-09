0700aed0e7a43bf75759e59fd692fddd8e30bc7b
Anton Micke <anton.micke@gmail.com>
2025-09-09 12:29:22 +0200

    Refactor

--- src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph1vs1View.java
@@ -4,23 +4,19 @@ import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.kickerelo.kickerelo.repository.Ergebnis1vs1Repository;
+import org.kickerelo.kickerelo.data.Spieler;
 import org.kickerelo.kickerelo.repository.SpielerRepository;
-import org.kickerelo.kickerelo.util.comparator.Spieler1vs1EloComparator;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 @Route("graph1vs1")
 public class Graph1vs1View extends VerticalLayout {
-    private final Ergebnis1vs1Repository ergebnisRepository;
     private final SpielerRepository spielerRepository;
     private Chart chart;
 
-    public Graph1vs1View(Ergebnis1vs1Repository ergebnisRepository, SpielerRepository spielerRepository) {
-        this.ergebnisRepository = ergebnisRepository;
+    public Graph1vs1View(SpielerRepository spielerRepository) {
         this.spielerRepository = spielerRepository;
         setSizeFull();
         H2 subheading = new H2("1 vs 1 Elo");
@@ -43,22 +39,12 @@ public class Graph1vs1View extends VerticalLayout {
         List<String> names = new ArrayList<>();
         List<Float> elo = new ArrayList<>();
 
-        spielerRepository.getSpielerWith1vs1Games().stream()
-                .filter((spieler) -> {
-                    if (!onlyActive) {
-                        return true;
-                    }
+        List<Spieler> spieler = (onlyActive) ?
+                spielerRepository.getSpielerWith1vs1GamesSince(LocalDateTime.now().minusWeeks(4))
+                : spielerRepository.getSpielerWith1vs1Games();
 
-                    var results = ergebnisRepository.getResultsForSpieler(spieler);
-                    return results.stream().anyMatch((result) -> {
-                        var twoWeeksAgo = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS).minusWeeks(2);
-                        return result.getTimestamp().isAfter(twoWeeksAgo);
-                    });
-                })
-                .sorted(new Spieler1vs1EloComparator()).forEach((s) -> {
-                    names.add(s.getName());
-                    elo.add(s.getElo1vs1());
-                });
+        spieler.forEach((s) -> {names.add(s.getName()); elo.add(s.getElo1vs1());});
+
         return new Chart(names, elo);
     }
 }
--- src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
+++ src/main/java/org/kickerelo/kickerelo/views/Graph2vs2View.java
@@ -4,23 +4,19 @@ import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.kickerelo.kickerelo.repository.Ergebnis2vs2Repository;
+import org.kickerelo.kickerelo.data.Spieler;
 import org.kickerelo.kickerelo.repository.SpielerRepository;
-import org.kickerelo.kickerelo.util.comparator.Spieler2vs2EloComparator;
 
 import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
 @Route("graph2vs2")
 public class Graph2vs2View extends VerticalLayout {
-    private final Ergebnis2vs2Repository ergebnisRepository;
     private final SpielerRepository spielerRepository;
     private Chart chart;
 
-    public Graph2vs2View(Ergebnis2vs2Repository ergebnisRepository, SpielerRepository spielerRepository) {
-        this.ergebnisRepository = ergebnisRepository;
+    public Graph2vs2View(SpielerRepository spielerRepository) {
         this.spielerRepository = spielerRepository;
         setSizeFull();
         H2 subheading = new H2("2 vs 2 Elo");
@@ -43,22 +39,12 @@ public class Graph2vs2View extends VerticalLayout {
         List<String> names = new ArrayList<>();
         List<Float> elo = new ArrayList<>();
 
-        spielerRepository.getSpielerWith2vs2Games().stream()
-                .filter((spieler) -> {
-                    if (!onlyActive) {
-                        return true;
-                    }
+        List<Spieler> spieler = (onlyActive) ?
+                spielerRepository.getSpielerWith2vs2GamesSince(LocalDateTime.now().minusWeeks(4))
+                : spielerRepository.getSpielerWith2vs2Games();
 
-                    var results = ergebnisRepository.getResultsForSpieler(spieler);
-                    return results.stream().anyMatch((result) -> {
-                        var twoWeeksAgo = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS).minusWeeks(2);
-                        return result.getTimestamp().isAfter(twoWeeksAgo);
-                    });
-                })
-                .sorted(new Spieler2vs2EloComparator()).forEach((s) -> {
-                    names.add(s.getName());
-                    elo.add(s.getElo2vs2());
-                });
+        spieler.forEach((s) -> {names.add(s.getName()); elo.add(s.getElo2vs2());});
+
         return new Chart(names, elo);
     }
 }
